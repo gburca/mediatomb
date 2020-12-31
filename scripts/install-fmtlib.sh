5f32d82761a15ff2f0b31708dd3d89c09a6df627
Gabriel Burca <gburca-git@ebixio.com>
2020-12-31 17:31:03 -0600

    Add install-fmtlib script

---
+++ scripts/install-fmtlib.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+if ! [ "$(id -u)" = 0 ]; then
+    echo "Please run this script with superuser access!"
+    exit 1
+fi
+set -ex
+
+VERSION=${VERSION:-"5.3.0"}
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+cleanup() {
+	rm "${script_dir}/fmt-$VERSION.tgz"
+	rm -rf "${script_dir}/fmt-$VERSION"
+}
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+unamestr=$(uname)
+if [ ! -f fmtlib-$VERSION.tgz ]; then
+	wget https://github.com/fmtlib/fmt/archive/$VERSION.tar.gz -O fmt-$VERSION.tgz
+fi
+tar -xzvf fmt-$VERSION.tgz
+cd fmt-$VERSION
+if [ -d build ]; then
+	rm -R build
+fi
+mkdir build
+
+# Should install in /usr/local
+cd build && \
+cmake .. && \
+make -j$(nproc) && \
+make install
