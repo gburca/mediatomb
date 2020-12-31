f9f5b60eaaf9783a8760982d77128d0e97800eb1
Gabriel Burca <gburca-git@ebixio.com>
2020-12-31 17:29:43 -0600

    Clean-up after spdlog install

--- scripts/install-spdlog.sh
+++ scripts/install-spdlog.sh
@@ -1,11 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
 if ! [ "$(id -u)" = 0 ]; then
     echo "Please run this script with superuser access!"
     exit 1
 fi
 set -ex
 
-VERSION="1.7.0"
+VERSION=${VERSION:-"1.7.0"}
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+cleanup() {
+	rm "${script_dir}/spdlog-$VERSION.tgz"
+	rm -rf "${script_dir}/spdlog-$VERSION"
+}
+trap cleanup SIGINT SIGTERM ERR EXIT
 
 unamestr=$(uname)
 if [ ! -f spdlog-$VERSION.tgz ]; then
